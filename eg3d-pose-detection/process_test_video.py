bfcb026fdd1f553a7ddc2909d3f2ae63cc15f475
bbaaii <40818542+bbaaii@users.noreply.github.com>
2023-07-03 21:39:31 +0800

    Update process_test_video.py

--- eg3d-pose-detection/process_test_video.py
+++ eg3d-pose-detection/process_test_video.py
@@ -14,7 +14,7 @@ print('Processing images:', sorted(glob.glob(os.path.join(args.input_dir, "*")))
 
 # Compute facial landmarks.
 print("Computing facial landmarks for model...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/batch_mtcnn.py"
+cmd = "python3.6 /eg3d-pose-detection/batch_mtcnn.py"
 input_flag = " --in_root " + args.input_dir
 cmd += input_flag
 # subprocess.run([cmd], shell=True, check=True)
@@ -22,14 +22,14 @@ os.system(cmd)
 
 
 print("Running smooth...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/smooth.py"
+cmd = "python3.6 /eg3d-pose-detection/smooth.py"
 input_flag = " --img_folder=" + args.input_dir
 cmd += input_flag
 os.system(cmd)
 
 # Run model inference to produce crops and raw poses.
 print("Running model inference...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/test.py"
+cmd = "python3.6 /eg3d-pose-detection/test.py"
 input_flag = " --img_folder=" + args.input_dir
 gpu_flag = " --gpu_ids=" + str(args.gpu) 
 model_name_flag = " --name=face_recon"
@@ -40,7 +40,7 @@ os.system(cmd)
 
 # Perform final cropping of 1024x1024 images.
 print("Processing final crops...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/crop_images.py"
+cmd = "python3.6 /eg3d-pose-detection/crop_images.py"
 input_flag = " --indir " + args.input_dir
 output_flag = " --outdir " + os.path.join(args.input_dir, 'cropped_images')
 cmd += input_flag + output_flag
@@ -49,7 +49,7 @@ os.system(cmd)
 
 # Process poses into our representation -- produces a cameras.json file.
 print("Processing final poses...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/3dface2idr.py"
+cmd = "python3.6 /eg3d-pose-detection/3dface2idr.py"
 input_flag = " --in_root " + os.path.join(args.input_dir, "epoch_20_000000")
 output_flag = " --out_root " + os.path.join(args.input_dir, "cropped_images")
 
@@ -58,7 +58,7 @@ cmd += input_flag + output_flag
 os.system(cmd)
 
 print("Transforming...")
-cmd = "python3.6 /apdcephfs_cq2/share_1290939/kitbai/eg3d-pose-detection-main-master/camera2label.py"
+cmd = "python3.6 /eg3d-pose-detection/camera2label.py"
 input_flag = " --in_root " + args.input_dir
 cmd += input_flag
 # subprocess.run([cmd], shell=True, check=True)
