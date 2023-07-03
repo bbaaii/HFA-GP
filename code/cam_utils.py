2bd769a7d3cedff31aa7c0e9bf46897b24a4f305
bbaaii <40818542+bbaaii@users.noreply.github.com>
2023-07-03 21:52:56 +0800

    Add files via upload

---
+++ code/cam_utils.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+# from networks.generator import Generator
+
+import numpy as np
+import torchvision
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+# from torch import nn, optim
+
+from torch.utils import data
+import torchvision.transforms as transforms
+
+
+import math
+
+import torchvision.utils as utils
+
+import wandb
+
+import mrcfile
+import torch.nn.functional as F
+
+
+
+
+def normalize_vecs(vectors: torch.Tensor) -> torch.Tensor:
+    """
+    Normalize vector lengths.
+    """
+    return vectors / (torch.norm(vectors, dim=-1, keepdim=True))
+
+def sample_camera_positions(device, n=1, r=1, horizontal_stddev=1, vertical_stddev=1, horizontal_mean=math.pi*0.5, vertical_mean=math.pi*0.5, mode='normal'):
+    """
+    Samples n random locations along a sphere of radius r. Uses the specified distribution.
+    Theta is yaw in radians (-pi, pi)
+    Phi is pitch in radians (0, pi)
+    """
+
+    if mode == 'uniform':
+        theta = (torch.rand((n, 1), device=device) - 0.5) * 2 * horizontal_stddev + horizontal_mean
+        phi = (torch.rand((n, 1), device=device) - 0.5) * 2 * vertical_stddev + vertical_mean
+
+    elif mode == 'normal' or mode == 'gaussian':
+        theta = torch.randn((n, 1), device=device) * horizontal_stddev + horizontal_mean
+        phi = torch.randn((n, 1), device=device) * vertical_stddev + vertical_mean
+
+    elif mode == 'hybrid':
+        if random.random() < 0.5:
+            theta = (torch.rand((n, 1), device=device) - 0.5) * 2 * horizontal_stddev * 2 + horizontal_mean
+            phi = (torch.rand((n, 1), device=device) - 0.5) * 2 * vertical_stddev * 2 + vertical_mean
+        else:
+            theta = torch.randn((n, 1), device=device) * horizontal_stddev + horizontal_mean
+            phi = torch.randn((n, 1), device=device) * vertical_stddev + vertical_mean
+
+    elif mode == 'truncated_gaussian':
+        theta = truncated_normal_(torch.zeros((n, 1), device=device)) * horizontal_stddev + horizontal_mean
+        phi = truncated_normal_(torch.zeros((n, 1), device=device)) * vertical_stddev + vertical_mean
+
+    elif mode == 'spherical_uniform':
+        theta = (torch.rand((n, 1), device=device) - .5) * 2 * horizontal_stddev + horizontal_mean
+        v_stddev, v_mean = vertical_stddev / math.pi, vertical_mean / math.pi
+        v = ((torch.rand((n,1), device=device) - .5) * 2 * v_stddev + v_mean)
+        v = torch.clamp(v, 1e-5, 1 - 1e-5)
+        phi = torch.arccos(1 - 2 * v)
+
+    else:
+        # Just use the mean.
+        theta = torch.ones((n, 1), device=device, dtype=torch.float) * horizontal_mean
+        phi = torch.ones((n, 1), device=device, dtype=torch.float) * vertical_mean
+
+    phi = torch.clamp(phi, 1e-5, math.pi - 1e-5)
+
+    output_points = torch.zeros((n, 3), device=device)
+    output_points[:, 0:1] = r*torch.sin(phi) * torch.cos(theta)
+    output_points[:, 2:3] = r*torch.sin(phi) * torch.sin(theta)
+    output_points[:, 1:2] = r*torch.cos(phi)
+
+    return output_points, phi, theta
+
+
+
+def create_cam2world_matrix(forward_vector, origin, device=None):
+    """Takes in the direction the camera is pointing and the camera origin and returns a cam2world matrix."""
+
+    forward_vector = normalize_vecs(forward_vector)
+    up_vector = torch.tensor([0, 1, 0], dtype=torch.float, device=device).expand_as(forward_vector)
+
+    left_vector = normalize_vecs(torch.cross(up_vector, forward_vector, dim=-1))
+
+    up_vector = normalize_vecs(torch.cross(forward_vector, left_vector, dim=-1))
+
+    rotation_matrix = torch.eye(4, device=device).unsqueeze(0).repeat(forward_vector.shape[0], 1, 1)
+    rotation_matrix[:, :3, :3] = torch.stack((-left_vector, up_vector, -forward_vector), axis=-1)
+
+    translation_matrix = torch.eye(4, device=device).unsqueeze(0).repeat(forward_vector.shape[0], 1, 1)
+    translation_matrix[:, :3, 3] = origin
+
+    cam2world = translation_matrix @ rotation_matrix
+
+    return cam2world
